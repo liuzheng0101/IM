de27a365287346646d2b94197250303d10ed2223
lz <1131010226@qq.com>
2019-04-07 16:26:37 +0800

    IM

---
+++ app/src/main/java/com/wd/tech/application/MyApplication.java
@@ -0,0 +1,14 @@
+package com.wd.tech.application;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+}
